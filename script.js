f10d7a7c6994564d1bfb67df85a1e6aefc2b23b8
IEYT <140979013+IEYT@users.noreply.github.com>
2026-05-23 11:13:19 -0600

    Add 'Do not click me' button with script & styles
    Introduce an interactive "Do not click me" button: create script.js (handles click messages and caps the message sequence), add the button markup to index.html and move its script include to the end of the page, and add corresponding styles in style.css for the button and container. Also include script.js in Games.html head so the script is available there.

---
+++ script.js
@@ -0,0 +1,23 @@
+let do_not_click_me = [
+  "Why did you click me?",
+  "I told you not to click me!",
+  "Seriously, stop clicking me!",
+  "Are you sure you want to keep clicking me?",
+  "Okay, that's it! I'm done!",
+];
+
+let click_count = 0;
+const doNotClickButton = document.getElementById("do-not-click-me");
+
+if (doNotClickButton) {
+  doNotClickButton.addEventListener("click", function () {
+    doNotClickButton.textContent =
+      do_not_click_me[Math.min(click_count, do_not_click_me.length - 1)];
+
+    click_count++;
+
+    if (click_count >= do_not_click_me.length) {
+      click_count = do_not_click_me.length - 1;
+    }
+  });
+}
